97a9bc83cd391cdc1b23448a8ad6ca9ddf37332e
digitalpurpl3e <118676093+digitalpurpl3e@users.noreply.github.com>
2022-12-02 16:50:23 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,33 +1,34 @@
-#include <Windows.h>
-#include <iostream>
+#include <Windows.h> //memory
+#include <iostream> //cout, cin
 
+
+// - http://www.club155.ru/x86cmd/Jcc
 int main()
 {
-	BYTE jump[] = { 0xEB, 0x00 }; 
-	DWORD adres = 0x7FF68679132F; //00007FF68679132F
-	DWORD pid;
+	BYTE jump[] = { 0xEB, 0x00 }; //jne - 0xEB, jmp - 0x00, nop - 0x90 
+	DWORD address = 0x0; 
+	DWORD pid; //processid
 
 	std::cout << "pid:";
 	std::cin >> pid;
-
-	HANDLE phandle = OpenProcess(PROCESS_ALL_ACCESS, true, pid);
+		
+	
+	//https://learn.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-openprocess
+	HANDLE phandle = OpenProcess(PROCESS_ALL_ACCESS, true, pid); 
 
 	if (!phandle)
 	{
 		std::cout << "error_!phandle" << std::endl;
 	}
+	
+	//https://learn.microsoft.com/en-us/windows/win32/api/memoryapi/nf-memoryapi-writeprocessmemory
+	if (WriteProcessMemory(phandle, LPVOID(address), &jump, sizeof(jump), 0))
+	{
+		std::cout << "success" << std::endl;
+	}
 	else
 	{
-		std::cout << "access" << std::endl;
-
-		if (!WriteProcessMemory(phandle, LPVOID(adres), &jump, sizeof(jump), 0))
-		{
-				std::cout << "error_WriteProcessMemory" << std::endl;
-		}
-		else
-		{
-			std::cout << "success" << std::endl;
-		}
+		std::cout << "error_WriteProcessMemory" << std::endl;
 	}
-	system("pause");
+	system("pause"); //getch
 }
